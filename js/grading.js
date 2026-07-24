092769170deab9093d71c5deea23aed53c17fd7b
loklok-source <loklim.tan@sumup.com>
2026-07-24 16:51:52 +0200

    Add generateTrainingFeedback function

--- js/grading.js
+++ js/grading.js
@@ -1,1 +1,14 @@
-
+function generateTrainingFeedback() {
+    return {
+        score: 88,
+        strengths: [
+            "Good greeting",
+            "Asked relevant questions",
+            "Correctly identified the issue"
+        ],
+        improvements: [
+            "Could show more empathy",
+            "Could explain next steps more clearly"
+        ]
+    };
+}
